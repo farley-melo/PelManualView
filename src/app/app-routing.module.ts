7a06fd826d462e62795f3eec0dabab8e9f1f0844
farley-melo <farleynirvana@gmail.com>
2021-02-18 01:17:45 -0300

    Inicio do layout de estoque

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'cadastro-estoque-materias-primas',loadChildren:()=>import('./cadastro/cadastro.module').then(m=>m.CadastroModule)}
+  {path:'cadastro-estoque-materias-primas',loadChildren:()=>import('./cadastro/cadastro.module').then(m=>m.CadastroModule)},
+  {path:'estoque',loadChildren:()=>import('./estoque/estoque.module').then(m=>m.EstoqueModule)}
 ];
 
 @NgModule({
---
+++ src/app/estoque/estoque.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EstoqueRoutingModule } from './estoque-routing.module';
+import { EstoqueComponent } from './estoque/estoque.component';
+
+
+@NgModule({
+  declarations: [EstoqueComponent],
+  imports: [
+    CommonModule,
+    EstoqueRoutingModule
+  ]
+})
+export class EstoqueModule { }
