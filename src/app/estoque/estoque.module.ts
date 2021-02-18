bc4c8911e364e189de220ae575f12ae7b7e8212a
farley-melo <farleynirvana@gmail.com>
2021-02-18 10:53:57 -0300

    Alteraçoes tela de cadastro de materias primas

--- src/app/estoque/estoque.module.ts
+++ src/app/estoque/estoque.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { EstoqueRoutingModule } from './estoque-routing.module';
 import { EstoqueComponent } from './estoque/estoque.component';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [EstoqueComponent],
-  imports: [
-    CommonModule,
-    EstoqueRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        EstoqueRoutingModule,
+        BsDropdownModule,FormsModule,ReactiveFormsModule
+    ]
 })
 export class EstoqueModule { }
