436092dac41bd86dfeb7fbbf776024855bed4d13
farley-melo <farleynirvana@gmail.com>
2021-02-19 11:08:20 -0300

    Incluido calculos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'cadastro-estoque-materias-primas',loadChildren:()=>import('./cadastro/cadastro.module').then(m=>m.CadastroModule)},
-  {path:'estoque',loadChildren:()=>import('./estoque/estoque.module').then(m=>m.EstoqueModule)}
+  {path:'estoque',loadChildren:()=>import('./estoque/estoque.module').then(m=>m.EstoqueModule)},
+  {path:'calculos',loadChildren:()=>import('./calculos/calculos.module').then(m=>m.CalculosModule)}
 ];
 
 @NgModule({
--- src/app/cadastro/cadastro-estoque-materias-primas/estoque.ts
+++ src/app/cadastro/cadastro-estoque-materias-primas/estoque.ts
@@ -2,4 +2,5 @@ export class Estoque {
   public nome:string;
   public  capacidade:number;
   public materiasPrimas:string[];
+  public status:string;
 }
